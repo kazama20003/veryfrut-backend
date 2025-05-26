1da9e05b958587b616183063d1967206a627cea5
kazama20003 <kazamatower@gmail.com>
2025-05-26 16:39:39 -0500

    port

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+
 @Controller('auth')
 export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
+
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
-    console.log('Petición recibida:', loginDto); // AÑADE ESTO
+    this.logger.log('➡️ Login llamado');
+    this.logger.debug(JSON.stringify(loginDto));
     return this.authService.login(loginDto);
+  }
+  @Post('register')
+  register(@Body() body: LoginDto) {
+    this.logger.log('➡️ Register' + body.email);
+    return { message: 'Registro exitoso' };
   }
 }
