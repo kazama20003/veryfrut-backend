81330c8321c9017d15f71ef7025cf5d34eec1a24
kazama2003 <kazamatower@gmail.com>
2026-02-05 03:18:13 -0500

    /orders

--- src/supliers/dto/create-suplier.dto.ts
+++ src/supliers/dto/create-suplier.dto.ts
@@ -1,1 +1,26 @@
-export class CreateSuplierDto {}
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class CreateSuplierDto {
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  companyName?: string;
+
+  @IsOptional()
+  @IsString()
+  contactName?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+}
